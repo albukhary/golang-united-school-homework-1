29621d6dcd9c62b9174994ff168f67ef9910edb7
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2022-02-15 00:14:50 +0500

    formatted

--- solution_test.go
+++ solution_test.go
@@ -2,12 +2,12 @@ package solution
 
 import (
 	"testing"
-) 
+)
 
 func TestGetMessage(t *testing.T) {
-    correct := "Hello 🗺️!"
+	correct := "Hello 🗺️!"
 	emoji := GetMessage()
 	if emoji != correct {
-       t.Errorf("Sum was incorrect, got: %v, want: %v.", emoji, correct)
-    }
+		t.Errorf("Sum was incorrect, got: %v, want: %v.", emoji, correct)
+	}
 }
